0c7ff87948a0f235ed54db0cda3944e3b936fcdb
tatsianova <ulyanava.ta@gmail.com>
2024-12-26 16:33:26 +0100

    Delete src/main/java/org/example/oop_tasks/examples/ex1 directory

--- src/main/java/org/example/oop_tasks/examples/ex1/Printable.java
+++
@@ -1,8 +0,0 @@
-package org.example.oop_tasks.examples.ex1;
-
-//Создайте интерфейс Printable с методом print(), который будет реализован в классе
-// BankAccount для вывода информации о счёте.
-public interface Printable {
-    // 'public' не нужен, так как в JAVA методы в интерфейсе по умолчанию public
-    void print();//фигурных скобок нет тк это интерфейс-мы не пишем в нем реализацию
-}
